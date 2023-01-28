a46ec03cee40500bf2c8f4e809289a473fdcb393
lucas cruz <lucas_cruz100@live.com>
2023-01-28 11:59:28 -0300

    first commit

---
+++ src/test/java/com/castgroup/auth/api/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castgroup.auth.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
